03caf3be543a414ad2bb21cded8242fb93173088
Tómas Rosdahl <torosdahl@gmail.com>
2015-12-08 21:18:10 +0100

    Cleanup filter bash files

--- scripts/cache_filter_clean.sh
+++ scripts/cache_filter_clean.sh
@@ -1,5 +1,7 @@
 temp_out=$(mktemp)
+mv $temp_out $temp_out.ipynb
 cat > $temp_out.ipynb
 jupyter nbconvert --to notebook --config scripts/convert_config.py $temp_out.ipynb --output $temp_out.ipynb 1>&2
 jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True $temp_out.ipynb --output $temp_out.ipynb 1>&2
 cat $temp_out.ipynb
+rm $temp_out.ipynb
