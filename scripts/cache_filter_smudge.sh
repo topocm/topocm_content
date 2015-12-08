4379966beda0bde3df0d71c592573c6f31a56a76
Tómas Rosdahl <torosdahl@gmail.com>
2015-12-08 08:58:04 +0100

    In place nbconvert of temp file

--- scripts/cache_filter_smudge.sh
+++ scripts/cache_filter_smudge.sh
@@ -1,7 +1,7 @@
 data=$(cat)
 temp_out=$(mktemp)
-echo "$data" > $temp_out
-jupyter trust $temp_out
-jupyter nbconvert --to notebook --config convert_config.py $temp_out --output $temp_out.ipynb
+echo "$data" > $temp_out.ipynb
+jupyter trust $temp_out.ipynb
+jupyter nbconvert --to notebook --config convert_config.py $temp_out.ipynb --output $temp_out.ipynb
 data=$(<$temp_out.ipynb)
 echo "$data"
