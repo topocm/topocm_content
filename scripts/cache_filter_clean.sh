f12b7d40520781c4c01b72b450c9857096ce1a3d
Tómas Rosdahl <torosdahl@gmail.com>
2015-12-09 09:00:41 +0100

    Modified clean filter

--- scripts/cache_filter_clean.sh
+++ scripts/cache_filter_clean.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 temp_out=$(mktemp)
-mv $temp_out $temp_out.ipynb
-cat > $temp_out.ipynb
-jupyter nbconvert --to notebook --config scripts/convert_config.py $temp_out.ipynb --output $temp_out.ipynb 1>&2
-jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True $temp_out.ipynb --output $temp_out.ipynb 1>&2
-cat $temp_out.ipynb
-rm $temp_out.ipynb
+cat > $temp_out
+jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True $temp_out --stdout
+rm $temp_out
