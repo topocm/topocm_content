e35105d669beb899434861b5d0a1fcacd3a9722c
Tómas Rosdahl <torosdahl@gmail.com>
2015-12-15 11:14:25 +0100

    Reintroduce %run in notebooks and conversion script

--- scripts/nb_py2to3.sh
+++ scripts/nb_py2to3.sh
@@ -2,8 +2,6 @@ cd ..
 p="$PWD"
 script_path="$p/scripts/ipython3-versioncontrol"
 point_dash="./"; dash="/"
-old="%run ../code/init_mooc_nb.py"
-new="exec(compile(open('../code/init_mooc_nb.py', 'rb').read(), '../code/init_mooc_nb.py', 'exec'))"
 
 find ./* -type d | while read -r line;
 do
@@ -15,8 +13,10 @@ do
 			python $script_path/notebook_v4_to_py.py -f "$file";
 			py_file_path="${file//.ipynb/.py}"
 			data=$(<"$py_file_path")
-			echo "${data//$old/$new}" > $py_file_path
+			echo "${data//%run/#%run}" > $py_file_path
 			2to3 -w -n "$py_file_path";
+			data=$(<"$py_file_path")
+			echo "${data//#%run/%run}" > $py_file_path
 			python $script_path/py_to_notebook_v4.py -f "$py_file_path" --overwrite;
 			rm "$py_file_path";
 		fi;
