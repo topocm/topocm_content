46c59e693030acb4f1dd1f150a1f506650cc9a57
Tómas Rosdahl <torosdahl@gmail.com>
2016-01-05 15:49:56 +0100

    Change and rename config files for filters

--- scripts/nb_filter_clean.sh
+++ scripts/nb_filter_clean.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 temp_out=$(mktemp)
-mv $temp_out $temp_out.ipynb
-cat > $temp_out.ipynb
-jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True $temp_out.ipynb --output $temp_out.ipynb 1>&2
-jupyter nbconvert --to notebook --config scripts/rmvers_config.py $temp_out.ipynb --stdout
+cat > $temp_out
+jupyter nbconvert --to notebook --config scripts/config_filter_clean.py $temp_out --stdout
 rm $temp_out.ipynb
