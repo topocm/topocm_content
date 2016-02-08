27abb83004edf61d414f710c26e451dd2f17dd0c
Tómas Rosdahl <torosdahl@gmail.com>
2016-02-08 10:33:58 +0100

    Insert newline at end if missing

--- scripts/nb_filter_smudge.sh
+++ scripts/nb_filter_smudge.sh
@@ -5,5 +5,6 @@ mv $temp_out $temp_out.ipynb
 cat > $temp_out.ipynb
 jupyter nbconvert --to notebook --config scripts/config_filter_smudge.py $temp_out.ipynb --output $temp_out.ipynb 1>&2
 jupyter trust $temp_out.ipynb > /dev/null
+sed -i '' -e '$a\' $temp_out.ipynb
 cat $temp_out.ipynb
 rm $temp_out.ipynb
