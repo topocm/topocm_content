415c39a63f4d9e42c90a07d8f380783c53eeeb3e
Anton Akhmerov <anton.akhmerov@gmail.com>
2016-02-08 08:04:24 +0100

    increase preprocessor timeout

--- scripts/config_filter_smudge.py
+++ scripts/config_filter_smudge.py
@@ -1,4 +1,7 @@
 c.Exporter.preprocessors = ['scripts.cachedoutput.CachedOutputPreprocessor']
 c.CachedOutputPreprocessor['enabled'] = True
 c.CachedOutputPreprocessor.cache_directory = '.nb_output_cache'
-c.CachedOutputPreprocessor.req_files = ['code/init_mooc_nb.py','code/edx_components.py','code/pfaffian.py']
+c.CachedOutputPreprocessor.req_files = ['code/init_mooc_nb.py',
+                                        'code/edx_components.py',
+                                        'code/pfaffian.py', 'code/functions.py']
+c.CachedOutputPreprocessor.timeout = 300
