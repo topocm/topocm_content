0b011a94dd41d1ebb93e9d4e14de2e5cc27a6284
Bas Nijholt <basnijholt@gmail.com>
2016-02-10 15:52:36 +0100

    Add newline to file

--- scripts/nb_filter_clean.sh
+++ scripts/nb_filter_clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 temp_out=$(mktemp)
 cat > $temp_out
+sed -i '' -e '$a\' $temp_out
 jupyter nbconvert --to notebook --config scripts/config_filter_clean.py $temp_out --stdout
 rm $temp_out
