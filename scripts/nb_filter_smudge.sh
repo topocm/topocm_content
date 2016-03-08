e801aee4b715e52c20c6eca7ddcde274518a8f8f
Tómas Rosdahl <torosdahl@gmail.com>
2016-03-08 17:12:40 +0100

    pipe from stdin in filters

--- scripts/nb_filter_smudge.sh
+++ scripts/nb_filter_smudge.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:${PWD}/code
-temp_out=$(mktemp)
-cat > $temp_out
-jupyter nbconvert --to notebook --config scripts/config_filter_smudge.py $temp_out --stdout
-rm $temp_out
+jupyter nbconvert --to notebook --config scripts/config_filter_smudge.py --stdin --stdout
