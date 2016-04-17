1736f9ee2eea63a7285376feac428fee9ca92a41
Thomas Jollans <tjol@tjol.eu>
2016-04-17 02:13:26 +0200

    make sure the clean and smudge filters are run in py3k
    Jupyter scripts may use the system python2 by default. The scripts
    require python3.

--- scripts/nb_filter_smudge.sh
+++ scripts/nb_filter_smudge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:${PWD}/code
-jupyter nbconvert --to notebook --config scripts/config_filter_smudge.py --stdin --stdout
+python3 -m nbconvert --to notebook --config scripts/config_filter_smudge.py --stdin --stdout
