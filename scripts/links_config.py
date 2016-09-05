55027370609a0ed6f1a699b62a66c5968d9c835e
Bas Nijholt <basnijholt@gmail.com>
2016-09-05 18:54:26 +0000

    Preprocess the notebooks for the site

--- scripts/links_config.py
+++ scripts/links_config.py
@@ -1,2 +1,6 @@
-c.Exporter.preprocessors = ['scripts.htmllinks.HtmlLinksPreprocessor']
+c.Exporter.preprocessors = ['scripts.htmllinks.HtmlLinksPreprocessor', 'scripts.cachedoutput.CachedOutputPreprocessor', 'scripts.trust_preprocessor.TrustPreprocessor']
 c.HtmlLinksPreprocessor['enabled'] = True
+c.CachedOutputPreprocessor['enabled'] = True
+c.CachedOutputPreprocessor.cache_directory = '.nb_output_cache'
+c.CachedOutputPreprocessor.timeout = 300
+c.TrustPreprocessor['enabled'] = True
