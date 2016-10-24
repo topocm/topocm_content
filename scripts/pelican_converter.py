b89bde788b815a788fc632c4e39cbb69afa4609e
Bas Nijholt <basnijholt@gmail.com>
2016-10-24 14:59:22 +0200

    Better titles and use current time

--- scripts/pelican_converter.py
+++ scripts/pelican_converter.py
@@ -6,6 +6,7 @@ import json
 import shutil
 import subprocess
 import nbformat
+import datetime
 
 
 def remove_first_cell(nb_name):
@@ -18,14 +19,17 @@ def remove_first_cell(nb_name):
     cells = cells[i:]
     return nbformat.v4.new_notebook(cells=cells, metadata={'name': cells[0].source[2:]})
 
-meta_file = """Title: Topocondmat.org
+
+meta_file = """Title: Topocondmat.org: {title}
 Slug: {slug}
-Date: 2010-12-03 10:20
+Date: {date}
 Category: """
 
+date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
 ipynbs = glob.glob('generated/with_output/w*/*.ipynb')
-figures = glob.glob('generated/with_output//w*/figures/*')
-print(ipynbs)
+figures = glob.glob('generated/with_output/w*/figures/*')
+
 try:
     shutil.rmtree('topocondmat/content')
     os.makedirs('topocondmat/content', exist_ok=True)
@@ -33,13 +37,14 @@ except:
     pass
 
 try:
-    shutil.rmtree('topocondmat/output')
+    shutil.rmtree('generated/html/')
 except:
     pass
 
 # Copy ipynbs and create meta data files
 for ipynb in ipynbs:
     nb = remove_first_cell(ipynb)
+    title = nb.cells[0].source[2:]
     new_fname = ipynb.replace('generated/with_output/', 'topocondmat/content/')
     os.makedirs(os.path.dirname(new_fname), exist_ok=True)
     with open(new_fname, 'w') as f:
@@ -47,13 +52,15 @@ for ipynb in ipynbs:
     with open(new_fname + '-meta', 'w', encoding='utf-8') as f:
         slug = ipynb.replace(
             'generated/with_output/', '').replace('.ipynb', '').replace('/', '-')
-        f.write(meta_file.format(slug=slug))
+        f.write(meta_file.format(slug=slug, title=title, date=date))
+
 
 # Copy figures
 for figure in figures:
-    new_fname = 'topocondmat/output/figures/' + os.path.basename(figure)
+    new_fname = 'generated/html/figures/' + os.path.basename(figure)
     os.makedirs(os.path.dirname(new_fname), exist_ok=True)
     shutil.copyfile(figure, new_fname)
+
 
 # Copy syllabus
 nb = remove_first_cell('syllabus.ipynb')
@@ -61,6 +68,5 @@ nb['cells'][1]['source'] = nb['cells'][1][
     'source'].replace('/', '-').replace('.ipynb', '.html')
 with open('topocondmat/content/syllabus.ipynb', 'w') as f:
     json.dump(nb, f)
-
 with open('topocondmat/content/syllabus.ipynb-meta', 'w', encoding='utf-8') as f:
-    f.write(meta_file.format(slug='syllabus'))
+    f.write(meta_file.format(slug='syllabus', title='Syllabus', date=date))
