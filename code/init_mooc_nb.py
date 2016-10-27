99b2eaeebc0d4add89b73cec28114f391adcad52
Bas Nijholt <basnijholt@gmail.com>
2016-10-27 14:11:20 +0000

    Remove code hiding

--- code/init_mooc_nb.py
+++ code/init_mooc_nb.py
@@ -10,7 +10,6 @@ import holoviews
 from holoviews import Options, Store
 from matplotlib import pyplot as plt
 from IPython import display
-from IPython.display import display_html
 import kwant
 
 import pfaffian as pf
@@ -71,46 +70,6 @@ def pretty_fmt_complex(num, digits=2):
             pretty_fmt_complex(num.imag) + 'i')
 
 
-nb_html_header = """
-<script type=text/javascript>
-/* Add a button for showing or hiding input */
-on = "Show input";
-off = "Hide input";
-function onoff(){
-  currentvalue = document.getElementById('onoff').value;
-  if(currentvalue == off){
-    document.getElementById("onoff").value=on;
-      $('div.input').hide();
-  }else{
-    document.getElementById("onoff").value=off;
-      $('div.input').show();
-  }
-}
-
-/* Launch first notebook cell on start */
-function launch_first_cell (evt) {
-  if (!launch_first_cell.executed
-      && Jupyter.notebook.kernel
-  ) {
-    Jupyter.notebook.get_cells()[0].execute();
-    launch_first_cell.executed = true;
-  }
-}
-
-$([Jupyter.events]).on('status_started.Kernel notebook_loaded.Notebook', launch_first_cell);
-</script>
-
-<p>Press this button to show/hide the code used in the notebook:
-<input type="button" class="ui-button ui-widget ui-state-default \
-ui-corner-all ui-button-text-only" value="Hide input" id="onoff" \
-onclick="onoff();"></p>
-"""
-
-hide_outside_ipython = """<script type=text/javascript>
-$(document).ready(function (){if(!("IPython" in window)){onoff();}})
-</script>"""
-
-
 def init_notebook(mpl=True):
     # Enable inline plotting in the notebook
     if mpl:
@@ -164,20 +123,6 @@ def init_notebook(mpl=True):
 
     np.set_printoptions(precision=2, suppress=True,
                         formatter={'complexfloat': pretty_fmt_complex})
-
-    # In order to make the notebooks readable through nbviewer we want to hide
-    # the code by default. However the same code is executed by the students,
-    # and in that case we don't want to hide the code. So we check if the code
-    # is executed by one of the mooc developers. Here we do by simply checking
-    # for some files that belong to the internal mooc repository, but are not
-    # published.  This is a temporary solution, and should be improved in the
-    # long run.
-
-    developer = os.path.exists(os.path.join(module_dir, os.path.pardir,
-                                            'scripts'))
-
-    display_html(display.HTML(nb_html_header +
-                              (hide_outside_ipython if developer else '')))
 
     # Patch a bug in holoviews
     if holoviews.__version__.release <= (1, 4, 3):
