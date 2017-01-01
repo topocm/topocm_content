fa99682a653c9d41555217d79457ed3e6d168db7
Anton Akhmerov <anton.akhmerov@gmail.com>
2017-01-01 15:41:11 +0100

    increase filter timeout

--- scripts/config_filter_smudge.py
+++ scripts/config_filter_smudge.py
@@ -4,5 +4,5 @@ c.CachedOutputPreprocessor.cache_directory = '.nb_output_cache'
 c.CachedOutputPreprocessor.req_files = ['code/init_mooc_nb.py',
                                         'code/edx_components.py',
                                         'code/pfaffian.py', 'code/functions.py']
-c.CachedOutputPreprocessor.timeout = 300
+c.CachedOutputPreprocessor.timeout = 3000
 c.TrustPreprocessor['enabled'] = True
