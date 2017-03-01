276dcbb7679008f9ea0f9128cdff089df7beff46
Bas Nijholt <basnijholt@gmail.com>
2017-03-01 17:33:58 +0100

    update iframeResizer

--- scripts/converter.py
+++ scripts/converter.py
@@ -40,7 +40,7 @@ cfg = Config({'HTMLExporter': {'template_file': 'edx',
 exportHtml = HTMLExporter(config=cfg)
 
 IFRAME_TEMPLATE = r"""
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.8/iframeResizer.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.10/iframeResizer.js"></script>
 
 <iframe id="{0}" scrolling="no" width="100%" frameborder=0>
 Your browser does not support IFrames.
@@ -50,7 +50,6 @@ Your browser does not support IFrames.
 var iframe = document.getElementById('{0}');
 iframe.src =  "//" + (document.domain.endsWith("edge.edx.org") ? "test." : "") + "topocondmat.org/edx/{0}.html?date=" + (+ new Date());
 </script>
-
 
 <script type="text/javascript">iFrameResize({heightCalculationMethod: isOldIE ? 'max' : 'lowestElement', minSize:100})</script>
 """
