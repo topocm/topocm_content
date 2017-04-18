9aedc49e22517a1dd99d6c697d7ad0da4aaa2edb
Bas Nijholt <basnijholt@gmail.com>
2017-04-18 19:55:34 +0200

    Disable ipython history

--- scripts/nbconvert.sh
+++ scripts/nbconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MPLCONFIGDIR=$(mktemp -d)
-jupyter nbconvert --HistoryManager.hist_file=$(mktemp) --to notebook --inplace --config scripts/config_filter_smudge.py $1
+jupyter nbconvert --HistoryManager.enabled=False --to notebook --inplace --config scripts/config_filter_smudge.py $1
