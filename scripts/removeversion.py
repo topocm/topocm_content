2dc3be575c921148104e303117aa8a3f025e7b59
Bas Nijholt <basnijholt@gmail.com>
2017-04-18 13:27:28 +0200

    Set Python 3 in metadata of notebook

--- scripts/removeversion.py
+++ scripts/removeversion.py
@@ -4,6 +4,12 @@ from nbconvert.preprocessors import Preprocessor
 class RemoveVersionPreprocessor(Preprocessor):
     
     def preprocess(self, nb, resources):
-    	if 'version' in nb.metadata['language_info']:
-    		del nb.metadata['language_info']['version']
-    	return nb, resources
+        if 'version' in nb.metadata['language_info']:
+            del nb.metadata['language_info']['version']
+        return nb, resources
+
+class SetNamePreprocessor(Preprocessor):
+    def preprocess(self, nb, resources):
+        nb.metadata['kernelspec']['display_name'] = "Python 3"
+        nb.metadata['kernelspec']['name'] = "python3"
+        return nb, resources
