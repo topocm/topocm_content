e67b366cac46a265ee22b9a2aef0c5d4c4e60930
Bas Nijholt <basnijholt@gmail.com>
2017-04-18 20:03:13 +0200

    Move setting to ipython config file

--- scripts/nbconvert.sh
+++ scripts/nbconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MPLCONFIGDIR=$(mktemp -d)
-jupyter nbconvert --HistoryManager.enabled=False --to notebook --inplace --config scripts/config_filter_smudge.py $1
+jupyter nbconvert --to notebook --inplace --config scripts/config_filter_smudge.py $1
