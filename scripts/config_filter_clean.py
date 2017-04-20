7002b57cdbae5b7a0e2233736c1543ff3a5ab7c9
Bas Nijholt <basnijholt@gmail.com>
2017-04-20 12:08:39 +0000

    Remove smudge filter from git filters

--- scripts/config_filter_clean.py
+++ scripts/config_filter_clean.py
@@ -1,4 +1,4 @@
 c.Exporter.preprocessors = ['scripts.removeversion.RemoveVersionPreprocessor',
-							'scripts.removeversion.SetNamePreprocessor']
+                            'scripts.removeversion.SetNamePreprocessor']
 c.ClearOutputPreprocessor['enabled'] = True
 c.RemoveVersionPreprocessor['enabled'] = True
