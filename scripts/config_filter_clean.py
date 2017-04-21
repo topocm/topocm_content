2520a7f7fafd121b9c672f1d21524d183dc564ee
Bas Nijholt <basnijholt@gmail.com>
2017-04-21 09:48:22 +0000

    Add longer timeout to nb execution

--- scripts/config_filter_clean.py
+++ scripts/config_filter_clean.py
@@ -2,3 +2,4 @@ c.Exporter.preprocessors = ['scripts.preprocessors.RemoveVersionPreprocessor',
                             'scripts.preprocessors.SetNamePreprocessor']
 c.ClearOutputPreprocessor['enabled'] = True
 c.RemoveVersionPreprocessor['enabled'] = True
+c.ExecutePreprocessor['timeout'] = 3000
