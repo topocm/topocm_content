d61ec08cfa0815a571978ac8c7a8ac24376c2fcf
Bas Nijholt <basnijholt@gmail.com>
2017-04-21 09:57:20 +0000

    Move timeout setting to cmd line interface of nbconvert

--- scripts/config_filter_clean.py
+++ scripts/config_filter_clean.py
@@ -2,4 +2,3 @@ c.Exporter.preprocessors = ['scripts.preprocessors.RemoveVersionPreprocessor',
                             'scripts.preprocessors.SetNamePreprocessor']
 c.ClearOutputPreprocessor['enabled'] = True
 c.RemoveVersionPreprocessor['enabled'] = True
-c.ExecutePreprocessor['timeout'] = 3000
--- scripts/nbconvert.sh
+++ scripts/nbconvert.sh
@@ -2,4 +2,4 @@
 export MPLCONFIGDIR=$(mktemp -d)
 export  IPYTHONDIR=$(mktemp -d)
 echo "c.HistoryManager.enabled = False" > $IPYTHONDIR/ipython_config.py
-jupyter nbconvert --to notebook --execute --inplace --config scripts/config_filter_smudge.py $1
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=3000 --execute --inplace --config scripts/config_filter_smudge.py $1
