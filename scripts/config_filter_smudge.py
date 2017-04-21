0d838b8473ad8aab25b96aca8cb4474439d83603
Bas Nijholt <basnijholt@gmail.com>
2017-04-21 09:43:44 +0000

    Combine preprocessors

--- scripts/config_filter_smudge.py
+++ scripts/config_filter_smudge.py
@@ -1,9 +1,2 @@
-c.Exporter.preprocessors = ['scripts.trust_preprocessor.TrustPreprocessor']
-# c.Exporter.preprocessors = ['scripts.cachedoutput.CachedOutputPreprocessor', 'scripts.trust_preprocessor.TrustPreprocessor']
-# c.CachedOutputPreprocessor['enabled'] = True
-# c.CachedOutputPreprocessor.cache_directory = '.nb_output_cache'
-# c.CachedOutputPreprocessor.req_files = ['code/init_mooc_nb.py',
-#                                         'code/edx_components.py',
-#                                         'code/pfaffian.py', 'code/functions.py']
-# c.CachedOutputPreprocessor.timeout = 3000
+c.Exporter.preprocessors = ['scripts.preprocessors.TrustPreprocessor']
 c.TrustPreprocessor['enabled'] = True
