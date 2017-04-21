e04d35e3383dffa17c3769f01c4115dd5fa506d1
Bas Nijholt <basnijholt@gmail.com>
2017-04-21 11:17:02 +0000

    Change stride for surface plots

--- code/init_mooc_nb.py
+++ code/init_mooc_nb.py
@@ -126,7 +126,7 @@ def init_notebook():
     options.Curve = Options('plot', aspect='square', title_format='{label}')
     options.Overlay = Options('plot', show_legend=False, title_format='{label}')
     options.Layout = Options('plot', title_format='{label}')
-    options.Surface = Options('style', cmap='RdBu_r', rstride=1, cstride=1,
+    options.Surface = Options('style', cmap='RdBu_r', rstride=2, cstride=2,
                               lw=0.2, edgecolors='k')
     options.Surface = Options('plot', azimuth=20, elevation=8)
 
