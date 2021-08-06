0c68f01f68aba4999eb0e4e95a653e8a4dfabb32
Anton Akhmerov <anton.akhmerov@gmail.com>
2021-08-06 23:25:53 +0200

    workaround of jb incorrectly overriding css

---
+++ src/_static/matomo.js
@@ -0,0 +1,11 @@
+var _paq = _paq || [];
+// tracker methods like "setCustomDimension" should be called before "trackPageView"
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//piwik.kwant-project.org/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', '6']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
