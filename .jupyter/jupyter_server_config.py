b3a5ff0622b54a126cab1b540fdbd30fc0cdf015
Anton Akhmerov <anton.akhmerov@gmail.com>
2025-11-21 16:33:44 +0100

    fix IPv6 vs IPv4

--- .jupyter/jupyter_server_config.py
+++ .jupyter/jupyter_server_config.py
@@ -57,6 +57,7 @@ class LimitingKernelManager(AsyncMappingKernelManager):
 
 
 c = get_config()  # noqa: F821
+c.ServerApp.ip = "127.0.0.1"  # Needed to specify explicitly to avoid binding to ::1.
 c.ServerApp.kernel_manager_class = LimitingKernelManager
 
 # Respect JUPYTER_NUM_PROCS environment variable if set. This allows CI (e.g.
